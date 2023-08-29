c609943f25677383c3ae729cd0bb86da543e963f
Sebastian <s.joist@gmx.net>
2023-08-29 12:34:15 +0200

    Rewriting scripts

--- scripts/manage.py
+++ scripts/manage.py
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     #installation.install(data_directory, args.Hostname)
 
     utils.copy_recursive(
-        data_directory, '/mnt/archinstall/tmp/', 777, ('root', 'root'), ignore=[])
+        root_directory, '/mnt/archinstall/tmp/', 777, ('root', 'root'), ignore=[])
 
     subprocess.run(
         'arch-chroot /mnt/archinstall python /tmp/drk-arch/scripts/configuration.py')

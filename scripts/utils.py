123bf07037ea68a8d0c1c9a575bc39dc67354f24
Sebastian Joist <s.joist@gmx.net>
2023-09-07 17:44:06 +0200

    m

--- scripts/utils.py
+++ scripts/utils.py
@@ -11,7 +11,7 @@ def copy_recursive(copy_src: str, copy_dst: str, dir_mode: int, ownership: tuple
         shutil.copyfile(copy_src)
 
     for root_dir, _, file_names in os.walk(copy_src, topdown=True):
-
+        print(root_dir)
         uid = pwd.getpwnam(ownership[0]).pw_uid
         gid = pwd.getpwnam(ownership[1]).pw_gid
         if not os.path.exists(root_dir):

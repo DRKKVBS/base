f6b36fa8cfaf03cfc73656a30d45370ca843196f
Sebastian Joist <s.joist@gmx.net>
2023-09-07 19:24:53 +0200

    m

--- scripts/setup_non_sudo.py
+++ scripts/setup_non_sudo.py
@@ -3,14 +3,17 @@ import os
 import shutil
 import subprocess
 
-def setup(file_directory:str):
+
+def setup(file_directory: str):
     with open(f'{file_directory}/setup.json', 'r') as f:
         setup_json = json.load(f)
         aur_pkgs = setup_json['aur_pkgs']
 
     # Install yay
     print('Installing yay')
-    # subprocess.Popen(args=["git", "clone", "https://aur.archlinux.org/yay", && cd yay/; makepkg -si --noconfirm; cd && rm -rf yay/"], group="admin", user="admin")
+    # subprocess.Popen(args=["git", "clone", "https://aur.archlinux.org/yay", & & cd yay/; makepkg - si - -noconfirm; cd & & rm - rf yay/"], group="admin", user="admin")
+    subprocess.run(
+        'git clone https://aur.archlinux.org/yay && cd yay/; makepkg -si --noconfirm; cd && rm -rf yay/')
 
     # Install third party packages
     for pkg in aur_pkgs:
@@ -25,9 +28,7 @@ def setup(file_directory:str):
             print(f'Installation of {pkg} was succesfull')
 
 
-
 if __name__ == "__main__":
     dwn_dir = os.path.realpath(
         os.path.dirname(__file__)).split('scripts')[0] + 'data'
     setup(dwn_dir)
-

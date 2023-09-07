60404a32ce780beb9ed6a5a9e9eba06d881ecd4e
Sebastian <s.joist@gmx.net>
2023-09-07 11:52:32 +0200

    m

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -3,7 +3,7 @@ import subprocess
 import setup_sudo
 
 
-def configure(file_directory):
+def configure(file_directory, script_directory):
 
     data_directory = os.path.join(file_directory, 'data')
 
@@ -16,4 +16,6 @@ def configure(file_directory):
 if __name__ == '__main__':
     root_directory = os.path.realpath(
         os.path.dirname(__file__)).split('scripts')[0]
-    configure(root_directory)
+    scripts_directory = os.path.realpath(
+        os.path.dirname(__file__)).split('scripts')[0] + 'scripts'
+    configure(root_directory, scripts_directory)

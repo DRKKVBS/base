6e4e8fb557d11d443a7def748ace4ca4bb37f2fd
Sebastian Joist <s.joist@gmx.net>
2023-09-07 17:41:06 +0200

    m

--- scripts/utils.py
+++ scripts/utils.py
@@ -10,19 +10,18 @@ def copy_recursive(copy_src: str, copy_dst: str, dir_mode: int, ownership: tuple
     if os.path.isfile(copy_src):
         shutil.copyfile(copy_src)
 
-    for root_dir, dir_names, file_names in os.walk(copy_src, topdown=True):
+    for root_dir, _, file_names in os.walk(copy_src, topdown=True):
 
         uid = pwd.getpwnam(ownership[0]).pw_uid
         gid = pwd.getpwnam(ownership[1]).pw_gidf
-        for dir in dir_names:
-            if not os.path.exists(root_dir):
-                print('Creating new directories for: ', root_dir)
-                os.mkdir(root_dir, mode=dir_mode)
-                os.chown(root_dir, uid=uid, gid=gid)
+        if not os.path.exists(root_dir):
+            print('Creating new directories for: ', root_dir)
+            os.mkdir(root_dir, mode=dir_mode)
+            os.chown(root_dir, uid=uid, gid=gid)
         for file in file_names:
             if len(ignore) > 0 and file in ignore:
                 continue
-            # print('Copy file: ', file, ' to ', path)
+            print('Copy file: ', file, ' to ', root_dir)
             shutil.copyfile(os.path.join(root_dir, file),
                             os.path.join(root_dir.replace(copy_src, copy_dst), file))
             os.chown(root_dir.replace(copy_src, copy_dst), uid=uid, gid=gid)

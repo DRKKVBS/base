c3ca648179f1487c12599da2776109b1785ad3c4
Sebastian <s.joist@gmx.net>
2023-09-29 17:42:47 +0200

    config refactoring

--- post_install/scripts/setup_non_sudo.sh
+++ post_install/scripts/setup_non_sudo.sh
@@ -3,8 +3,8 @@ cd /home/admin
 pwd
 env
 git clone https://aur.archlinux.org/yay
-cd yay/ && makepkg -si --noconfirm
-cd ~ && rm -rf yay/
+cd ./yay/ && makepkg -si --noconfirm
+cd ./ && rm -rf ./yay/
 for pkg in "$@"; do
     yay -S $pkg --noconfirm
 done

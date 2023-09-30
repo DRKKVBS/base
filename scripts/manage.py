9832947cf948bfd1630797d68156aea840208ce0
Sebastian <s.joist@gmx.net>
2023-09-30 11:25:12 +0200

    config refactoring

--- scripts/manage.py
+++ scripts/manage.py
@@ -93,7 +93,7 @@ if __name__ == '__main__':
     #    ['arch-chroot' '-u', 'admin', '/mnt/archinstall', 'python', '/home/admin/drk-arch/scripts/setup_non_sudo.py'], shell=False)
     print(post_install_json["aur_pkgs"])
     subprocess.run(
-        ['arch-chroot', '-u', 'admin', '/mnt/archinstall', '/usr/bin/sudo', '-i', '-u', 'admin', '/home/admin/drk-arch/scripts/setup_non_sudo.sh'], shell=False, text=True)
+        ['arch-chroot', '-u', 'admin', '/mnt/archinstall/', '/usr/bin/sudo', '-i', '-u', 'admin', '/home/admin/drk-arch/scripts/setup_non_sudo.sh'], shell=False, text=True)
 
     # subprocess.run(
     #     ['arch-chroot', '/mnt/archinstall', '/home/admin/drk-arch/scripts/setup_sudo.py', post_install_json["aur_pkgs"]], shell=False, text=True)

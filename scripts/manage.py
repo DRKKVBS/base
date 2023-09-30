8360c470f096a02f4caf187bcdb69ff5df16869e
Sebastian <s.joist@gmx.net>
2023-09-30 11:24:16 +0200

    config refactoring

--- scripts/manage.py
+++ scripts/manage.py
@@ -95,8 +95,8 @@ if __name__ == '__main__':
     subprocess.run(
         ['arch-chroot', '-u', 'admin', '/mnt/archinstall', '/usr/bin/sudo', '-i', '-u', 'admin', '/home/admin/drk-arch/scripts/setup_non_sudo.sh'], shell=False, text=True)
 
-    subprocess.run(
-        ['arch-chroot', '/mnt/archinstall', '/home/admin/drk-arch/scripts/setup_sudo.py', post_install_json["aur_pkgs"]], shell=False, text=True)
+    # subprocess.run(
+    #     ['arch-chroot', '/mnt/archinstall', '/home/admin/drk-arch/scripts/setup_sudo.py', post_install_json["aur_pkgs"]], shell=False, text=True)
 # Delete Downloaded git repo
 # shutil.rmtree(os.path.realpath(
 #     os.path.dirname(__file__)).split('scripts')[0])

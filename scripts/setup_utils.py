11964878c4aa0a19ad262c636ecc20290b70aa49
Sebastian Joist <s.joist@gmx.net>
2023-10-06 16:14:24 +0200

    m

--- scripts/setup_utils.py
+++ scripts/setup_utils.py
@@ -110,12 +110,12 @@ def desktop_apps(desktop_app_dirs: str, user: str, uid: int, gid: int, visible_a
             f"/mnt/archinstall/home/{user}/.local/share/applications/", uid=uid, gid=gid)
 
     make_mutable(f"/home/{user}/.local/share/applications/")
-    # Copy the Desktop Files into the new directory
-    shutil.copytree(
-        desktop_app_dirs, "/mnt/archinstall/home/%s/.local/share/applications/" % user, dirs_exist_ok=True)
     for file in os.listdir(f"/mnt/archinstall/home/{user}/.local/share/applications/"):
         make_mutable(
             f"/home/{user}/.local/share/applications/{file}")
+    # Copy the Desktop Files into the new directory
+    shutil.copytree(
+        desktop_app_dirs, "/mnt/archinstall/home/%s/.local/share/applications/" % user, dirs_exist_ok=True)
 
     # Make Dekstop Entries hidden
     for file in os.listdir("/mnt/archinstall/usr/share/applications/"):

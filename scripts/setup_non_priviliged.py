8725315b5522cf8eaf3abe63ae720246c837848b
Sebastian Joist <s.joist@gmx.net>
2023-10-06 11:40:03 +0200

    m

--- scripts/setup_non_priviliged.py
+++ scripts/setup_non_priviliged.py
@@ -18,7 +18,7 @@ def install_aur_package(chroot: bool, package: str):
 def install_yay():
     for cmd in ['git clone https://aur.archlinux.org/yay /home/admin/yay/', 'cd ./yay/ && makepkg -si --noconfirm; cd ./; rm -rf ./yay/']:
         pass
-    subprocess.run("arch-chroot -u admin:admin /mnt/archinstall sudo -i -u admin /bin/bash -c 'pwd'",
+    subprocess.run("arch-chroot -u admin:admin /mnt/archinstall sudo -i -u admin /bin/bash -c 'pwd && whoami; pwd'",
                    shell=True)
 
 

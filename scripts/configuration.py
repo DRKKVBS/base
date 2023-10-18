8be239df681978c2270af6cac57203f1767c34db
Sebastian <s.joist@gmx.net>
2023-10-18 09:52:31 +0200

    m

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -38,17 +38,15 @@ def configure(root_directory: str):
     with open(f"{root_directory}/data/copy.json", "r") as f:
         data = json.load(f)
         for k, v in data.items():
-            print(f"Source: {root_directory}/data/{v.get('source')}",
-                                        f"Destination: /mnt/archinstall/{v.get('destination')}")
             try:
                 if os.path.isdir(f"{root_directory}/data/{v.get('source')}"):
                     for dst in v.get('destination'):
                         shutil.copytree(f"{root_directory}/data/{v.get('source')}",
-                                        f"/mnt/archinstall/{v.get('destination')}")
+                                        f"/mnt/archinstall/{dst}")
                 elif os.path.isfile(f"{root_directory}/data/{v.get('source')}"):
                     for dst in v.get('destination'):
                         shutil.copyfile(f"{root_directory}/data/{v.get('source')}",
-                                        f"/mnt/archinstall/{v.get('destination')}")
+                                        f"/mnt/archinstall/{dst}")
             except Exception as e:
                 print(e)
                 pass

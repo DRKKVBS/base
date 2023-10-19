6dd69ba5b3f45514cd078ef66c5868ed1b2cf0c0
Sebastian <s.joist@gmx.net>
2023-10-19 12:22:04 +0200

    m

--- scripts/configuration.py
+++ scripts/configuration.py
@@ -30,31 +30,29 @@ def configure(data: dict, users: dict):
     finally:
         setup_utils.reenable_sudo_password("admin")
 
-    with open(f"{root_directory}/data/copy.json", "r") as f:
-        data = json.load(f)
-        for k, v in data.items():
+    for k, v in data.items():
 
-            try:
-                print(type(v.get('destination')))
-                print(f"{root_directory}{v.get('source')}")
+        try:
+            print(type(v.get('destination')))
+            print(f"{root_directory}{v.get('source')}")
 
-                if os.path.isdir(f"{root_directory}{v.get('source')}"):
+            if os.path.isdir(f"{root_directory}{v.get('source')}"):
 
-                    for dst in v.get('destination'):
-                        print(f"Destination: /mnt/archinstall{dst}")
-                        shutil.copytree(f"{root_directory}{v.get('source')}",
-                                        f"/mnt/archinstall{dst}", dirs_exist_ok=True)
+                for dst in v.get('destination'):
+                    print(f"Destination: /mnt/archinstall{dst}")
+                    shutil.copytree(f"{root_directory}{v.get('source')}",
+                                    f"/mnt/archinstall{dst}", dirs_exist_ok=True)
 
-                elif os.path.isfile(f"{root_directory}{v.get('source')}"):
+            elif os.path.isfile(f"{root_directory}{v.get('source')}"):
 
-                    for dst in v.get('destination'):
-                        print(f"Destination: /mnt/archinstall{dst}")
-                        shutil.copyfile(f"{root_directory}{v.get('source')}",
-                                        f"/mnt/archinstall{dst}")
+                for dst in v.get('destination'):
+                    print(f"Destination: /mnt/archinstall{dst}")
+                    shutil.copyfile(f"{root_directory}{v.get('source')}",
+                                    f"/mnt/archinstall{dst}")
 
-            except Exception as e:
-                print(e)
-                pass
+        except Exception as e:
+            print(e)
+            pass
 
     for user, data in users.items():
         if user == 'admin':

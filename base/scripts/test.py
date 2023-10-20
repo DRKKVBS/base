a6a58188d3072775ed432467b938732e14909b45
Sebastian <s.joist@gmx.net>
2023-10-20 15:13:18 +0200

    m

--- base/scripts/test.py
+++ base/scripts/test.py
@@ -10,5 +10,5 @@ print_color = Color()
 if not os.path.exists('/mnt/archinstall/home/admin/.local/share/applications') and len(os.listdir('/mnt/archinstall/home/admin/.local/share/applications')):
     print_color.print_error('AccountServices are not setup')
 
-if len(os.listdir('/mnt/archinstall/home/admin/.local/share/applications')):
+else len(os.listdir('/mnt/archinstall/home/admin/.local/share/applications')):
     print_color.print_error('AccountServices are not setup')

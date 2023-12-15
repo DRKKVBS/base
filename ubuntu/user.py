b3726042fb13b3f0e1a75a2b309e4303677f20d1
Sebastian Joist <s.joist@gmx.net>
2023-12-15 16:16:00 +0100

    Ubuntu rewrite

--- ubuntu/user.py
+++ ubuntu/user.py
@@ -12,13 +12,17 @@ class User():
         self.sudo = sudo
         self.dekstop_entries = dekstop_entries
         self.create_user()
+        # Get system data of the user
+        user = pwd.getpwnam(self.username)  # type: ignore
+        self.uid = user.pw_uid
+        self.gid = user.pw_gid
+        self.home_dir = user.pw_dir
 
     def create_user(self):
         """Create a user."""
 
         # Check if user exists
         if self.user_exists():
-            self.get_user_data()
             return
 
         # Create user
@@ -38,9 +42,6 @@ class User():
         except Exception as e:
             print("User Creation failed: %s" % e)
 
-        else:
-            self.get_user_data()
-
     def user_exists(self) -> bool:
         """Check if a user exists."""
         try:
@@ -48,12 +49,3 @@ class User():
             return True
         except KeyError:
             return False
-
-    def get_user_data(self):
-        """Get system data of the user"""
-
-        # Get system data of the user
-        user = pwd.getpwnam(self.username)  # type: ignore
-        self.uid = user.pw_uid
-        self.gid = user.pw_gid
-        self.home_dir = user.pw_dir

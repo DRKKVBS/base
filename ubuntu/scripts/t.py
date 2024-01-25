7d2d53d46595d34cfac08b5c6a674ca2f018bfb1
Sebastian Joist <s.joist@gmx.net>
2024-01-25 20:03:32 +0100

    m

--- ubuntu/scripts/t.py
+++ ubuntu/scripts/t.py
@@ -1,36 +1,18 @@
-from subprocess import PIPE, STDOUT, Popen, run
-
-# -*- coding: utf-8 -*-
-"""
-A convenience module for shelling out with realtime output
-includes: 
-- subprocess - Works with additional processes.
-- shlex - Lexical analysis of shell-style syntaxes.
-"""
-
+from __future__ import print_function  # Only Python 2.x
 import subprocess
-import shlex
 
 
-def run(command):
-    process = Popen(command, stdout=PIPE, shell=True)
-    while True:
-        line = process.stdout.readline().rstrip()
-        if not line:
-            break
-        yield line.decode('utf-8')
+def execute(cmd):
+    popen = subprocess.Popen(
+        cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
 
-def run_command(command):
-    process = Popen(shlex.split(command), stdout=PIPE)
-    while True:
-        output = process.stdout.readline().rstrip().decode('utf-8')
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            print(output.strip())
-    rc = process.poll()
-    return rc
 
-if __name__ == "__main__":
-    for path in run("ping -c 5 google.com"):
-        print path
+# Example
+for path in execute(["locate", "a"]):
+    print(path, end="")

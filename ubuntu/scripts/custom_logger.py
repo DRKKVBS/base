e52d53d5dc69e6f8e46626d207f8d646425f21dc
Sebastian Joist <s.joist@gmx.net>
2024-01-25 21:01:31 +0100

    Added porting init.sh to python code

--- ubuntu/scripts/custom_logger.py
+++ ubuntu/scripts/custom_logger.py
@@ -10,7 +10,7 @@ def setup_logging() -> logging.Logger:
     # Setup logging
     logger = logging.getLogger("config_logger")
 
-    with open(os.path.normpath(f"{currrent_dir}/logging_config.json"), "r") as f:
+    with open(os.path.normpath(f"{currrent_dir}/script_configs/logging_config.json"), "r") as f:
         logging.config.dictConfig(config=json.load(f))
 
     return logger

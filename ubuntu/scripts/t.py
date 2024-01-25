de6abbc998ab34f3be767d0051c1e116fe21faf2
Sebastian Joist <s.joist@gmx.net>
2024-01-25 20:20:10 +0100

    m

--- ubuntu/scripts/t.py
+++ ubuntu/scripts/t.py
@@ -6,7 +6,7 @@ def execute(cmd):
     popen = subprocess.Popen(
         cmd, stdout=subprocess.PIPE, universal_newlines=True, text=True)
     for stdout_line in iter(popen.stdout.readline, ""):
-        yield stdout_line
+        print(stdout_line, end="")
     popen.stdout.close()
     return_code = popen.wait()
     if return_code:
@@ -14,5 +14,4 @@ def execute(cmd):
 
 
 # Example
-for path in execute(["apt", "update"]):
-    print(path, end="")
+execute(["apt", "update"])

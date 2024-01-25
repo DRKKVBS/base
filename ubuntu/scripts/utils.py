b531dc130b7e26f928a2822bb79e63cbe16a1b08
Sebastian Joist <s.joist@gmx.net>
2024-01-25 19:35:34 +0100

    m

--- ubuntu/scripts/utils.py
+++ ubuntu/scripts/utils.py
@@ -150,7 +150,7 @@ def run_command(cmds: list):
     try:
         logger.info(f"Executing: {cmds}")
         r = subprocess.run([*cmds],
-                           stdout=subprocess.PIPE, text=True, shell=False)
+                           stdout=subprocess.PIPE, shell=False, bufsize=1)
 
         if r.returncode != 0:
             logger.warning(

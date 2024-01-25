6ddb3bcafaa8f1e2a8dc8b0660af8262cf986c89
Sebastian Joist <s.joist@gmx.net>
2024-01-25 21:15:28 +0100

    Added porting init.sh to python code

--- ubuntu/scripts/init.py
+++ ubuntu/scripts/init.py
@@ -2,30 +2,37 @@ import json
 import utils
 import os
 import main
+from getpass import getpass
 
 
 root_dir = utils.get_root_dir()
 
-utils.run_command(["apt", "update"])
-utils.run_command(["snap", "remove", "--purge", "firefox"])
-utils.run_command(["apt", "upgrade", "-y"])
-
-# Install packages from the packages directory
-for pkg in os.listdir(os.path.normpath(f"{root_dir}/packages/")):
-    utils.install_package(pkg)
-
-utils.run_command(["apt", "update"])
-
 # Install packages from the packages directory
 with open(os.path.normpath(f"{root_dir}/script_configs/config.json")) as f:
     data = json.load(f)
-    # Promt the user to enter a hostname if none is set
-    if data["hostname"] == None:
-        data["hostname"] = input(
-            "Please enter a hostname for the system and press enter to continue...")
-    print(data["hostname"])
-    # for pkg in data["apt_packages"]:
-    # utils.install_package(pkg)
+
+# Promt the user to enter a hostname if none is set
+if data["hostname"] == None:
+    data["hostname"] = input(
+        "Please enter a hostname for the system and press enter to continue...\n")
+
+if data["users"]["admin"]["password"] == None:
+    data["users"]["admin"]["password"] = getpass(
+        "Please enter a hostname for the system and press enter to continue...\n")
+
+# utils.run_command(["apt", "update"])
+# utils.run_command(["snap", "remove", "--purge", "firefox"])
+# utils.run_command(["apt", "upgrade", "-y"])
+
+# # Install packages from the packages directory
+# for pkg in os.listdir(os.path.normpath(f"{root_dir}/packages/")):
+#     utils.install_package(pkg)
+
+# utils.run_command(["apt", "update"])
+
+
+# for pkg in data["apt_packages"]:
+#     utils.install_package(pkg)
 
 
 # utils.run_command(["apt", "update"])

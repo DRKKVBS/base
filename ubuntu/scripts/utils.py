b56b006c27a82d2a6e5842fb26c8fb6d1625a029
Sebastian Joist <s.joist@gmx.net>
2024-01-25 21:24:19 +0100

    Added porting init.sh to python code

--- ubuntu/scripts/utils.py
+++ ubuntu/scripts/utils.py
@@ -236,10 +236,51 @@ def add_desktop_app(user: User, visible_apps: list):
                                  "[Desktop Entry]\nNoDisplay=True")
 
 
-def test():
-    logger.info("Hello Info!")
-    logger.error("Hello Error!")
-    pass
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+            It must be "yes" (the default), "no" or None (meaning
+            an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == "":
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write(
+                "Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
+
+
+def input_validation(question: str):
+    """Ask a for input via input() and return their answer. The user has to enter the same input again to confirm.
+
+    "question" is a string that is presented to the user.
+    """
+
+    while True:
+        input1 = input(question+"\n")
+        input2 = input("Please reenter the the password:\n")
+        if input1 == input2:
+            return input1
+        else:
+            print("Yout inputs do not match. Please try again.\n")
 
 # def hide_desktop_app(app: str, user: User):
 #     '''Hide a desktop app from user so he cannot access via the acitvities screen.'''

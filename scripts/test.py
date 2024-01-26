16bff2458d780e5c6c6958685df1c3ed4f85c75c
xWannaDieQuickly <s.joist@gmx.net>
2024-01-26 16:39:46 +0100

    User class update

--- scripts/test.py
+++ scripts/test.py
@@ -19,33 +19,17 @@ except Exception as e:
     logger.error(f"Error loading config file: {e}")
     exit(1)
 
-# Promt the user to enter a hostname if none is set
-if data["hostname"] == None:
-    data["hostname"] = input(
-        "Please enter a hostname for the system and press enter to continue...\n")
-    logger.info(f"Hostname set to {data['hostname']}")
 
-with open("/etc/hostname", "w") as f:
-    f.write(data["hostname"] + "\n")
-
-
-if data["users"]["admin"]["password"] == None:
-    data["users"]["admin"]["password"] = helper.input_validation(
-        "Please enter a password for the Administrator account and press enter to continue...")
-    logger.info(
-        f"Administrator password set to {data['users']['admin']['password']}")
-
-
-helper.run_command(["apt", "update"])
-helper.run_command(["snap", "remove", "--purge", "firefox"])
+# helper.run_command(["apt", "update"])
+# helper.run_command(["snap", "remove", "--purge", "firefox"])
 
 # Install packages from the packages directory
 for pkg in os.listdir(os.path.normpath(f"{root}/packages/")):
     print(pkg)
     pkg_helper.install_package(pkg)
 
-helper.run_command(["apt", "update"])
+# helper.run_command(["apt", "update"])
 
 
-for pkg in data["packages"]["install"]:
-    pkg_helper.install_package(pkg)
+# for pkg in data["packages"]["install"]:
+#     pkg_helper.install_package(pkg)

3976aa8c7db723d512c59ea9435c01eadf94c636
Sebastian Joist <s.joist@gmx.net>
2024-01-28 15:43:20 +0100

    porting init.sh to python code

--- scripts/citrix.sh
+++ scripts/citrix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND="noninteractive"
-sudo debconf-set-selections <<<"icaclient app_protection/install_app_protection select yes"
-sudo debconf-show icaclient
+debconf-set-selections <<<"icaclient app_protection/install_app_protection select yes"
+debconf-show icaclient

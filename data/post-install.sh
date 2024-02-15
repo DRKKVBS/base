77fc859f99ef69c49db4ab732699d2922a05b277
Sebastian Joist <s.joist@gmx.net>
2024-02-15 13:06:53 +0100

    replaces run_command

--- data/post-install.sh
+++ data/post-install.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Delete the default user
 userdel -r drk
 
+# Delete the public wifi connection
 if test -f /etc/NetworkManager/system-connections/DRK_SAK_Mobile.nmconnection; then
     rm /etc/NetworkManager/system-connections/DRK_SAK_Mobile.nmconnection
 fi
 
-teamviewer --daemon start &
+# Start Teamviewer
+teamviewer --daemon start
+teamviewer &
 
+# Assign the device to the account
 teamviewer assignment --id
 
+# Delete the script
 rm "$0"
-
-# systemctl enable displaylink
-# systemctl start displaylink

f5f13c7bfaf0601288066152146aa90e332bb675
Sebastian <s.joist@gmx.net>
2024-03-25 19:51:30 +0100

    Rewrite in Bash

--- data/test.sh
+++ data/test.sh
@@ -2,4 +2,4 @@
 
 sudo apt install jq -y
 
-echo '{"drk-logo": {"source": "/logos/drk-logo.png", "destination": "/usr/share/drk/drk-logo.png", "comment": "Logo for the login screen"}, "x11-vty-switch": {"source": "/01-vt-switch.conf","destination": "/etc/X11/xorg.conf.d/01-vt-switch.conf","comment": "Disable Virtual Terminals"}}' | jq '.drk-logo'
+echo '{"drk-logo": {"source": "/logos/drk-logo.png", "destination": "/usr/share/drk/drk-logo.png", "comment": "Logo for the login screen"}}' | jq '.drk-logo'
